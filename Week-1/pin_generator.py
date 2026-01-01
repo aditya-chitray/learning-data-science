acd1c65272861cd5af41067d3b99bf16da7bf59a
aditya-chitray <chitray.16@gmail.com>
2026-01-01 17:29:21 +0530

    pin generator updated

--- Week-1/pin_generator.py
+++ Week-1/pin_generator.py
@@ -1,17 +1,23 @@
-# This program extracts pin from a given poem by the rule that nth digit of the pin is length of the nth word in the nth line
-
-def pin_extractor(poem):
-    secret_code = ''
-    lines = poem.split('\n')
-    for line_index, line in enumerate(lines):  # provides both index and value
-        words = line.split()
-        secret_code += str(len(words[line_index]))
-    
-    return secret_code
-
+def pin_extractor(poems):
+    secret_codes = []
+    for poem in poems:
+        secret_code = ''
+        lines = poem.split('\n')
+        for line_index, line in enumerate(lines):
+            words = line.split()
+            if len(words) > line_index:
+                secret_code += str(len(words[line_index]))
+            else:
+                secret_code += '0'
+        secret_codes.append(secret_code)
+    return secret_codes
 
 poem = """Stars and the moon
 shine in the sky
-white and bright
+white and
 until the end of the night"""
-print(pin_extractor(poem))
+
+poem2 = 'The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow'
+poem3 = 'There\nonce\nwas\na\ndragon'
+
+print(pin_extractor([poem,poem2,poem3]))
